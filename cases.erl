54a318072c35566c4cd1ab49e647e209e0af7b1f
otofu-square <yukita@feedforce.jp>
2017-07-23 18:40:56 +0900

    :+1: Add cases module

---
+++ cases.erl
@@ -0,0 +1,20 @@
+-module(cases).
+
+-export([beach/1, insert/2]).
+
+insert(X, []) -> [X];
+insert(X, Set) ->
+    case lists:member(X, Set) of
+      true -> Set;
+      false -> [X | Set]
+    end.
+
+beach(Temperature) ->
+    case Temperature of
+      {celsius, N} when N >= 20, N =< 45 -> favorable;
+      {kelvin, N} when N >= 293, N =< 318 ->
+	  'scientifically favorable';
+      {fahrenheit, N} when N >= 68, N =< 113 ->
+	  'favorable in the US';
+      _ -> 'avoid beech'
+    end.
