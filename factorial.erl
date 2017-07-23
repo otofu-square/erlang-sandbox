76a3b849ac35d6490025710b36884ba87c0097c8
otofu-square <yukita@feedforce.jp>
2017-07-23 22:31:37 +0900

    :recycle: Improve factorial by tail calling

--- factorial.erl
+++ factorial.erl
@@ -1,6 +1,11 @@
 -module(factorial).
 
--export([factorial/1]).
+-export([fac/1, tail_fac/1, tail_fac/2]).
 
-factorial(0) -> 1;
-factorial(N) when N >= 0 -> N * factorial(N - 1).
+fac(0) -> 1;
+fac(N) when N >= 0 -> N * fac(N - 1).
+
+tail_fac(N) -> tail_fac(N, 1).
+
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) -> tail_fac(N - 1, N * Acc).
