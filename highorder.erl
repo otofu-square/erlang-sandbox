0c9d97aceffb53d59fd6868f16cb28d461711c92
otofu-square <yukita@feedforce.jp>
2017-07-25 01:02:47 +0900

    :+1: Add filter function on highorder module

--- highorder.erl
+++ highorder.erl
@@ -1,6 +1,7 @@
 -module(highorder).
 
--export([map/2, tail_map/2, tail_map/3]).
+-export([filter/2, map/2, tail_filter/2, tail_filter/3,
+	 tail_map/2, tail_map/3]).
 
 map(_, []) -> [];
 map(F, [X | Xs]) -> [F(X) | map(F, Xs)].
@@ -10,3 +11,19 @@ tail_map(F, L) -> tail_map(F, L, []).
 tail_map(_, [], Acc) -> Acc;
 tail_map(F, [X | Xs], Acc) ->
     tail_map(F, Xs, Acc ++ [F(X)]).
+
+filter(_, []) -> [];
+filter(F, [X | Xs]) ->
+    case F(X) of
+      true -> [X | filter(F, Xs)];
+      false -> filter(F, Xs)
+    end.
+
+tail_filter(F, L) -> tail_filter(F, L, []).
+
+tail_filter(_, [], Acc) -> Acc;
+tail_filter(F, [X | Xs], Acc) ->
+    case F(X) of
+      true -> tail_filter(F, Xs, Acc ++ X);
+      false -> tail_filter(F, Xs, Acc)
+    end.
